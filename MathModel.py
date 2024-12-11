0168a4fafd93b74e3e399352f85f05d38e116623
janrems <jan1.rems@gmail.com>
2024-12-11 16:55:51 +0100

    LQ examples tested

--- MathModel.py
+++ MathModel.py
@@ -1,6 +1,6 @@
 import numpy as np
 import torch
-
+from scipy.integrate import odeint
 
 #LQ model normal jump
 class LQJump:
@@ -102,10 +102,10 @@ class IBContinuous:
 
     #diffusion (dim_x x dim_d)
     def sigma(self, t, x_t, mu_t, u_t):
-        sigma = torch.tensor([self.vol * self.rho, self.vol * np.sqrt(1 - self.rho ** 2)])
+        sigma = torch.tensor([self.vol * self.rho, self.vol * np.sqrt(1 - self.rho ** 2)], dtype=torch.float32)
         sigma = torch.repeat_interleave(sigma.unsqueeze(0), self.dim_x, 0)
         sigma = torch.repeat_interleave(sigma.unsqueeze(0), x_t.shape[0], 0)
-        return sigma.float()
+        return sigma
 
     def forward_step(self, t, x_t, mu_t, u_t, dW, dP):
         dx = self.b(t, x_t, mu_t, u_t) * self.dt + torch.matmul(self.sigma(t, x_t, mu_t, u_t), dW).view(x_t.shape[0],
@@ -156,7 +156,7 @@ class IBContinuous:
         x_t = x_0
         mu_t = cex_seq_true[:, 0, :]
 
-        ongoing_cost = torch.zeros(x_0.shape[0])
+        ongoing_cost = self.f(0, x_t, mu_t, u_t) * self.dt
 
         for i in range(self.N - 1):
             t = self.dt * i
@@ -210,10 +210,10 @@ class IBJump:
 
     #diffusion (dim_x x dim_d)
     def sigma(self, t, x_t, mu_t, u_t):
-        sigma = torch.tensor([self.vol * self.rho, self.vol * np.sqrt(1 - self.rho ** 2)])
+        sigma = torch.tensor([self.vol * self.rho, self.vol * np.sqrt(1 - self.rho ** 2)], dtype=torch.float32)
         sigma = torch.repeat_interleave(sigma.unsqueeze(0), self.dim_x, 0)
         sigma = torch.repeat_interleave(sigma.unsqueeze(0), x_t.shape[0], 0)
-        return sigma.float()
+        return sigma
 
     # jump (dim_x x dim_n)
     def gamma(self, t, x_t, mu_t, u_t):
@@ -277,7 +277,7 @@ class IBJump:
         x_t = x_0
         mu_t = cex_seq_true[:, 0, :]
 
-        ongoing_cost = torch.zeros(x_0.shape[0])
+        ongoing_cost = self.f(0, x_t, mu_t, u_t)*self.dt
 
         for i in range(self.N - 1):
             t = self.dt * i
@@ -336,10 +336,10 @@ class IBCommonJump:
 
     # jump (dim_x x dim_n)
     def gamma(self, t, x_t, mu_t, u_t):
-        gamma = torch.tensor([self.rho, 1 - self.rho])
+        gamma = torch.tensor([self.rho, 1 - self.rho], dtype=torch.float32)
         gamma = torch.repeat_interleave(gamma.unsqueeze(0), self.dim_x, 0)
         gamma = torch.repeat_interleave(gamma.unsqueeze(0), x_t.shape[0], 0)
-        return gamma.float()
+        return gamma
 
     # Compensator
     def compensator(self, size):
@@ -369,3 +369,254 @@ class IBCommonJump:
     #Loss function
     def loss(self, ongoing_cost, terminal_cost):
         return torch.mean(ongoing_cost + terminal_cost, dim=0)
+
+
+class LQJump:
+    def __init__(self, T, N, dim_x, dim_w, dim_n, theta, vol, rates,
+                 jump_means, jump_sds, jump_type, common_noise, path):
+        self.T = T
+        self.N = N
+        self.dt = T / N
+
+        self.dim_x = dim_x
+        self.dim_w = dim_w
+        self.dim_n = dim_n
+
+        self.theta = theta
+        self.vol = vol
+
+        self.rates = rates
+        self.jump_means = jump_means
+        self.jump_sds = jump_sds
+        self.jump_type = jump_type
+        self.common_noise = common_noise
+
+        self.path = path
+
+    #initial value is drawn from U(0,1)
+    def initial(self, size):
+        return torch.rand(size, self.dim_x)
+
+    #drift (dim_x)
+    def b(self, t, x_t, mu_t, u_t):
+        return u_t
+
+    #diffusion (dim_x x dim_d)
+    def sigma(self, t, x_t, mu_t, u_t):
+        sigma = torch.tensor([self.theta, self.vol], dtype=torch.float32)
+        sigma = torch.repeat_interleave(sigma.unsqueeze(0), self.dim_x, 0)
+        sigma = torch.repeat_interleave(sigma.unsqueeze(0), x_t.shape[0], 0)
+
+        ones = torch.ones(x_t.shape[0], self.dim_x, 1)
+        tmp = torch.cat((ones,mu_t.unsqueeze(-1)), dim=2)
+        return sigma*tmp
+
+
+    # jump (dim_x x dim_n)
+    def gamma(self, t, x_t, mu_t, u_t):
+        return mu_t.unsqueeze(-1)
+
+    # Compensator
+    def compensator(self, size):
+        expected_jumps = torch.tensor(self.rates) * torch.tensor(self.jump_means)
+        expected_jumps = torch.repeat_interleave(expected_jumps.unsqueeze(0), size, 0)
+        return expected_jumps.unsqueeze(-1)
+
+    def forward_step(self, t, x_t, mu_t, u_t, dW, dP):
+        dx = (self.b(t, x_t, mu_t, u_t) * self.dt +
+              torch.matmul(self.sigma(t, x_t, mu_t, u_t), dW).view(x_t.shape[0], self.dim_x) +
+              torch.matmul(self.gamma(t, x_t, mu_t, u_t), dP).view(x_t.shape[0], self.dim_x) -
+              torch.matmul(self.gamma(t, x_t, mu_t, u_t),
+                           self.compensator(x_t.shape[0])).view(x_t.shape[0], self.dim_x) * self.dt)
+        return x_t + dx
+
+    #Ongoing cost
+    def f(self, t, x_t, mu_t, u_t):
+        return 0.5 * torch.linalg.norm(u_t, dim=-1) ** 2
+
+
+    #Terminal cost
+    def g(self, x_T, mu_T):
+        return 0.5*torch.linalg.norm(x_T, dim=-1) ** 2
+
+    #Loss function
+    def loss(self, ongoing_cost, terminal_cost):
+        return torch.mean(ongoing_cost + terminal_cost, dim=0)
+
+    def explicit_solution(self, dW, dP, common_noise, state_seq):
+        if self.dim_x != 1:
+            raise ValueError("Explicit solution only implemented for dim_x = 1")
+
+        def riccati_sys(y, t, theta, vol, mu, sd, l):
+            k0, k1, k2, k3 = y
+            dyt = [-theta ** 2 * (k1 + k2 + k3), -0.5 * (2 * k1 + k2) ** 2, -2 * (2 * k1 + k2) * (k2 + 2 * k3),
+                   -0.5 * (k2 + 2 * k3) ** 2 - k1 * (vol ** 2 + l * (mu ** 2 + sd ** 2))]
+            return dyt
+
+        size = dW.shape[0]
+        yT = [0.0, -0.5, 0.0, 0.0]
+        tb = np.linspace(self.T, 0, self.N)
+
+        sol = odeint(riccati_sys, yT, tb, args=(self.theta, self.vol, self.jump_means[0],
+                                                self.jump_sds[0], self.rates[0]))
+        sol = np.flip(sol, 0)
+        sol = sol.T
+
+        sol = np.expand_dims(sol, axis=1)
+        sol = np.repeat(sol, size, axis=1)
+        sol = np.expand_dims(sol, axis=-1)
+
+        k0, k1, k2, k3 = torch.tensor(sol, dtype=torch.float32)
+        k = 2 * (k1 + k2 + k3)
+
+        state_seq_true = state_seq.clone()
+        x_0 = state_seq_true[:, 0, :]
+
+        ex_0 = torch.mean(x_0[:, 0], dim=0)
+        cex_seq_true = torch.ones(size, self.N, self.dim_x) * ex_0
+
+        control_seq_true = (2 * k1 + k2) * state_seq_true + (k2 + 2 * k3) * cex_seq_true
+
+        u_t = control_seq_true[:, 0, :]
+        x_t = x_0
+        mu_t = cex_seq_true[:, 0, :]
+
+        ongoing_cost = self.f(0, x_t, mu_t, u_t) * self.dt
+
+        for i in range(self.N - 1):
+            t = self.dt * i
+            x_t = self.forward_step(t, x_t, mu_t, u_t, dW[:, i, :, :], dP[:, i, :, :])
+            mu_t = mu_t + k[:, i, :]*mu_t*self.dt + self.theta*(common_noise[:, i + 1, :] - common_noise[:, i, :])
+            u_t = (2 * k1 + k2)[:, i + 1, :] * x_t + (k2 + 2 * k3)[:, i + 1, :] * mu_t
+            ongoing_cost += self.f(t, x_t, mu_t, u_t) * self.dt
+
+            state_seq_true[:, i + 1, :] = x_t
+            control_seq_true[:, i + 1, :] = u_t
+            cex_seq_true[:, i + 1, :] = mu_t
+
+        terminal_cost = self.g(x_t, mu_t)
+        loss_true = self.loss(ongoing_cost, terminal_cost)
+
+        return state_seq_true, control_seq_true, cex_seq_true, loss_true
+
+
+class LQCommonJump:
+    def __init__(self, T, N, dim_x, dim_w, dim_n, rates, jump_means, jump_sds, jump_type, common_noise, path):
+        self.T = T
+        self.N = N
+        self.dt = T / N
+
+        self.dim_x = dim_x
+        self.dim_w = dim_w
+        self.dim_n = dim_n
+
+        self.rates = rates
+        self.jump_means = jump_means
+        self.jump_sds = jump_sds
+        self.jump_type = jump_type
+        self.common_noise = common_noise
+
+        self.path = path
+
+    #initial value is drawn from U(0,1)
+    def initial(self, size):
+        return torch.rand(size, self.dim_x)
+
+    #drift (dim_x)
+    def b(self, t, x_t, mu_t, u_t):
+        return u_t
+
+
+
+
+    # jump (dim_x x dim_n)
+    def gamma(self, t, x_t, mu_t, u_t):
+        ones = torch.ones(x_t.shape[0], self.dim_x, 1)
+        gamma = torch.cat((ones, mu_t.unsqueeze(-1)), dim=2)
+        return gamma
+
+    # Compensator
+    def compensator(self, size):
+        expected_jumps = torch.tensor(self.rates) * torch.tensor(self.jump_means)
+        expected_jumps = torch.repeat_interleave(expected_jumps.unsqueeze(0), size, 0)
+        return expected_jumps.unsqueeze(-1)
+
+    def forward_step(self, t, x_t, mu_t, u_t, dW, dP):
+        dx = (self.b(t, x_t, mu_t, u_t) * self.dt +
+              torch.matmul(self.gamma(t, x_t, mu_t, u_t), dP).view(x_t.shape[0], self.dim_x) -
+              torch.matmul(self.gamma(t, x_t, mu_t, u_t),
+                           self.compensator(x_t.shape[0])).view(x_t.shape[0], self.dim_x) * self.dt)
+        return x_t + dx
+
+    #Ongoing cost
+    def f(self, t, x_t, mu_t, u_t):
+        return 0.5 * torch.linalg.norm(u_t, dim=-1) ** 2
+
+
+    #Terminal cost
+    def g(self, x_T, mu_T):
+        return 0.5*torch.linalg.norm(x_T, dim=-1) ** 2
+
+    #Loss function
+    def loss(self, ongoing_cost, terminal_cost):
+        return torch.mean(ongoing_cost + terminal_cost, dim=0)
+
+    def explicit_solution(self, dW, dP, common_noise, state_seq):
+        if self.dim_x != 1:
+            raise ValueError("Explicit solution only implemented for dim_x = 1")
+
+        def riccati_sys(y, t, mu, sd, l):
+            k0, k1, k2, k3, k4, k5 = y
+            dyt = [-(0.5 * (k4 + k5) ** 2 + (k1 + k3) * l * (mu ** 2 + sd ** 2) + (k5 - k2) * l * mu + k1 * l * (
+                        mu ** 2 + sd ** 2)), -0.5 * (2 * k1 + k2) ** 2, - (2 * k1 + k2) * (k2 + 2 * k3),
+                   -0.5 * (k2 + 2 * k3) ** 2, - ((2 * k1 + k2) * (k4 + k5) + k2 * l * mu),
+                   - ((k2 + 2 * k3) * (k4 + k5) + (2 * k3 - k2) * l * mu - k2 * l * mu)]
+            return dyt
+
+        size = dW.shape[0]
+        yT = [0.0, -0.5, 0.0, 0.0,0.0,0.0]
+        tb = np.linspace(self.T, 0, self.N)
+
+        sol = odeint(riccati_sys, yT, tb, args=(self.jump_means[0],
+                                                self.jump_sds[0], self.rates[0]))
+        sol = np.flip(sol, 0)
+        sol = sol.T
+
+        sol = np.expand_dims(sol, axis=1)
+        sol = np.repeat(sol, size, axis=1)
+        sol = np.expand_dims(sol, axis=-1)
+
+        k0, k1, k2, k3, k4, k5 = torch.tensor(sol, dtype=torch.float32)
+        k = 2 * (k1 + k2 + k3)
+
+
+
+        state_seq_true = state_seq.clone()
+        x_0 = state_seq_true[:, 0, :]
+
+        ex_0 = torch.mean(x_0[:, 0], dim=0)
+        cex_seq_true = torch.ones(size, self.N, self.dim_x) * ex_0
+
+        control_seq_true = (2 * k1 + k2) * state_seq_true + (k2 + 2 * k3) * cex_seq_true + k4 + k5
+
+        u_t = control_seq_true[:, 0, :]
+        x_t = x_0
+        mu_t = cex_seq_true[:, 0, :]
+
+        ongoing_cost = self.f(0, x_t, mu_t, u_t) * self.dt
+
+        for i in range(self.N - 1):
+            t = self.dt * i
+            x_t = self.forward_step(t, x_t, mu_t, u_t, dW[:, i, :, :], dP[:, i, :, :])
+            mu_t = mu_t + (k[:, i, :]*mu_t + k4[:, i, :] + k5[:, i, :])*self.dt + (common_noise[:, i + 1, :] - common_noise[:, i, :])
+            u_t = (2 * k1 + k2)[:, i + 1, :] * x_t + (k2 + 2 * k3)[:, i + 1, :] * mu_t + k4[:, i + 1, :] + k5[:, i + 1, :]
+            ongoing_cost += self.f(t, x_t, mu_t, u_t) * self.dt
+
+            state_seq_true[:, i + 1, :] = x_t
+            control_seq_true[:, i + 1, :] = u_t
+            cex_seq_true[:, i + 1, :] = mu_t
+
+        terminal_cost = self.g(x_t, mu_t)
+        loss_true = self.loss(ongoing_cost, terminal_cost)
+
+        return state_seq_true, control_seq_true, cex_seq_true, loss_true
--- Tests.py
+++ Tests.py
@@ -5,6 +5,9 @@ import matplotlib.pyplot as plt
 from MathModel import IBContinuous
 from MathModel import IBJump
 from MathModel import IBCommonJump
+from MathModel import LQJump
+from MathModel import LQCommonJump
+
 from DeepSolver import DeepSolver
 
 
@@ -17,18 +20,29 @@ ib_j = IBJump(T=1.0, N=100, dim_x=1, dim_w=2, dim_n=1, reversion=1.0, vol=0.2, r
                     path="C:/Users/jan1r/Documents/Faks/Doktorat/CommonNoise/Graphs/NewGraphs/ib_j/")
 
 ib_cj = IBCommonJump(T=1.0, N=100, dim_x=1, dim_w=1, dim_n=2, reversion=1.0, vol=0.2, rho=0.2, q=1.0, epsilon=1.5,
-                    c=1.0, rates=[5.0, 5.0], jump_means=[0.0, 0.0], jump_sds=[0.05, 0.05], jump_type="Normal", common_noise="Poisson",
+                    c=1.0, rates=[5.0, 5.0], jump_means=[0.0, 0.0], jump_sds=[0.05, 0.05], jump_type="Normal",
+                    common_noise="Poisson",
                     path="C:/Users/jan1r/Documents/Faks/Doktorat/CommonNoise/Graphs/NewGraphs/ib_cj/")
 
 
-solver = DeepSolver(math_model=ib_cj, batch_size=2**10, eval_size=2**10, learning_rate=0.0005, n_epochs=4, ridge_param=0.1, depth=2)
+lq_j = LQJump(T=1.0, N=100, dim_x=1, dim_w=2, dim_n=1, theta=0.3, vol=0.2, rates=[5.0], jump_means=[0.0],
+              jump_sds=[0.05], jump_type="Normal", common_noise="Brownian",
+              path="C:/Users/jan1r/Documents/Faks/Doktorat/CommonNoise/Graphs/NewGraphs/lq_j/")
+
+lq_cj = LQCommonJump(T=1.0, N=100, dim_x=1, dim_w=1, dim_n=2, rates=[5.0, 5.0], jump_means=[0.0, 0.0],
+              jump_sds=[0.05, 0.05], jump_type="Normal", common_noise="Poisson",
+              path="C:/Users/jan1r/Documents/Faks/Doktorat/CommonNoise/Graphs/NewGraphs/lq_cj/")
+
+
+
+solver = DeepSolver(math_model=lq_cj, batch_size=2**10, eval_size=2**10, learning_rate=0.0005, n_epochs=200, ridge_param=0.1, depth=2)
 
 solver.train()
 
 state_seq, control_seq, cex_seq, loss, dW, dP, common_noise = solver.eval()
 
 
-state_seq_true, control_seq_true, cex_seq_true, loss_true = ib_j.explicit_solution(dW, dP, common_noise, state_seq)
+state_seq_true, control_seq_true, cex_seq_true, loss_true = lq_cj.explicit_solution(dW, dP, common_noise, state_seq)
 
 solver.plot_loss(loss_true)
 
